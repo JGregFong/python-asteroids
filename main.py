fc07167aba842a41990877717b4a8b63e5ed3a32
JGregFong <jgregfong@gmail.com>
2026-02-17 23:41:40 -0800

    Updated gitignore, circleshape, constants, player, asteroid, and main files. Also, added shot file.

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from logger import log_event
+from shot import Shot
+import sys
+
 import pygame
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids with pygame version: VERSION")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    Asteroid.containers = (asteroids,updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    Shot.containers = (shots, updatable, drawable)
+
+    asteroid_field = AsteroidField()
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+
 
     while True:
         log_state()
@@ -14,7 +41,29 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        dt = clock.tick(60)/1000
+        for item in drawable:
+            item.draw(screen)
+
+        updatable.update(dt)
+        for rock in asteroids:
+            if(player.collides_with(rock)):
+                log_event("player_hit")
+                print("Game Over")
+                sys.exit()
+            for shot in shots:
+                if(shot.collides_with(rock)):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    rock.kill()
+
+
+
         pygame.display.flip()
+
+
+
+        #print(dt)
 
 if __name__ == "__main__":
     main()
